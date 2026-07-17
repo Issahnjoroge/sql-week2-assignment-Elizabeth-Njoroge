685c75e66f5e5899568c3bd4e1c924ae93b58666
Elizabeth Njoroge <123112233+Issahnjoroge@users.noreply.github.com>
2026-07-17 18:46:42 +0300

    Add SQL queries for student and exam results

---
+++ section_c_query.sql
@@ -0,0 +1,21 @@
+select * from students
+where class = 'Form_4';
+
+
+select * from subjects
+where department = 'Sciences';
+
+
+select * from exam_results
+where marks >= '70';
+
+
+select * from students
+where gender = 'F';
+
+
+select * from students
+where class = 'Form 3' and city = 'Nairobi';
+
+select * from students
+where class = 'Form 2' or class = 'Form 4';
